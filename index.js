5c65d6ecdf144095d505d1c2b885a5e79eee4e4c
Кристина Медянникова <72264341+Kristina930@users.noreply.github.com>
2022-11-27 16:54:52 +0300

    lesson3

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-/*Решите задачу по выводу данных в консоль*/
-Ответ 
-/*Record 1
-Record 5
-Record 6
-Record 2
-Первое выполниться console.log('Record 1') и console.log('Record 5')
-Вторые выведються как имеют приеоретет  Promise Record 6.
-Третьем будет выведено setTimeout Record 2*/
 
-console.log('Record 1');
+/*По ссылке вы найдёте файл с логами запросов к серверу весом более 2 Гб. Напишите программу,
+которая находит в этом файле все записи с ip-адресами  и 34.48.240.111, а также
+сохраняет их в отдельные файлы с названием %ip-адрес%_requests.log*/
 
-    setTimeout(() => {
-        console.log('Record 2');
-    Promise.resolve().then(() => {
-        setTimeout(() => {
-            сonsole.log('Record 3');
-    Promise.resolve().then(() => {
-        console.log('Record 4');
-            });
-        });
-    });
-});
 
-console.log('Record 5');
+const fs = require('fs');
+const readline = require('readline');
 
-Promise.resolve().then(() => Promise.resolve().then(() => console.log('Record 6')));
+const readFile = fs.createReadStream('access_tmp.log', 'utf-8');
+
+const ip1 = '89.123.1.41';
+const ip2 = '34.48.240.111';
+
+const file1 = fs.createReadStream(`${ip1}`);
+const file2 = fs.createReadStream(`${ip2}`);
+
+let numberStr = 0;
+
+const fileLine = readline.createInterface({
+    input: readFile
+})
+
+fileLine.on('line', (line) => {
+    if (line.includes(ip1)) {
+        file1.write(line + '\n')
+    }
+    if (line.includes(ip2)) {
+        file2.write(line + '\n')
+    }
+
+    console.log(++numberStr);
+})
 
 
 
